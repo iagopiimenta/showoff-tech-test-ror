496b76393d62a01f17248a2b4120e42c275d6718
Iago <iagopimenta.s@gmail.com>
2020-03-07 19:10:57 +0000

    Add showoff api envs

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module ShowoffTechTestRor
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.x.showoff = config_for(:showoff).symbolize_keys
   end
 end
