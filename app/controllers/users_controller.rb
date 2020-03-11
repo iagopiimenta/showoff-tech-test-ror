9f0aa50c4787f5fc0f463f8d723d7ff0f9ed5793
Iago <iagopimenta.s@gmail.com>
2020-03-11 20:39:16 +0000

    Allow create an user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,55 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-
   # TODO
   def new
 
   end
 
-  # TODO
   def create
+    @user = User.new(user_params)
 
+    if @user.create!
+      token_data = ShowoffOauth.convert_token_to_hash(@user.token)
+      session[:user] = token_data
+
+      redirect_to root_path
+    else
+      redirect_to root_path, flash: { error: @user.errors.full_messages.to_sentence }
+    end
   end
+
+  def sign_in
+    token = ShowoffOauth.new.create_token(
+      sign_in_params[:email],
+      sign_in_params[:password]
+    )
+
+    token_data = ShowoffOauth.convert_token_to_hash(token)
+
+    session[:user] = token_data
+
+    redirect_to root_path
+  rescue OAuth2::Error => e
+    # {"code":3,"message":"There was an error logging in. Please try again.","data":null}
+    redirect_to root_path, flash: { error: "Invalid email or password" }
+  end
+
+  def sign_out
+    reset_session
+
+    redirect_to root_path
+  end
+
+  private
+
+    def sign_in_params
+      params.require(:user).permit(:email, :password)
+    end
+
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :description,
+        :kind
+      )
+    end
 end
